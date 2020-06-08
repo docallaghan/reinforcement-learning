a80bf72f59ac3013c88d97e85ef24f7dba4d227c
David O'Callaghan <ocallaghan1@gmail.com>
2020-06-08 12:34:56 +0100

    Added DQN agent.

--- item_gathering_gridworld.py
+++ item_gathering_gridworld.py
@@ -9,7 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-DEBUG = True
+import tensorflow as tf
+from tensorflow import keras
+
+from collections import deque # Used for replay buffer and reward tracking
+from datetime import datetime # Used for timing script
+
+
+DEBUG = False
 
 GRID_ROWS = 8
 GRID_COLS = 8
@@ -22,6 +29,7 @@ RED_REWARD = 10
 GREEN_REWARD = 11
 YELLOW_REWARD = 12
 TRANSITION_REWARD = -1
+
 
 class ItemGatheringGridworld:
     
@@ -178,6 +186,9 @@ class ItemGatheringGridworld:
         return self.n_steps >= 50
     
     def __initialise_grid_display(self, boundaries):
+        """
+        Set up the plot objects for displaying an the gridworld image.
+        """
         # Set axis limits
         self.ax.set_xlim(-0.5, self.grid_cols - 0.5)
         self.ax.set_ylim(-0.5, self.grid_rows - 0.5)
@@ -197,7 +208,6 @@ class ItemGatheringGridworld:
         # Invert y-axis - (0,0) at top left instead of bottom left
         self.ax.set_ylim(self.ax.get_ylim()[::-1])
         
-        
         # Display image
         self.image = self.ax.imshow(self.grid)
         
@@ -212,20 +222,196 @@ class ItemGatheringGridworld:
         plt.draw()
         plt.pause(0.05)
 
-        
-if __name__ == '__main__':
-    np.random.seed(23)
-    env = ItemGatheringGridworld()
-    env.show(boundaries=True)
-    print(env.get_current_state())
-    print()
+
+class DQNAgent:
     
-    for i in range(100):
-        state, reward, done = env.step(random.choice([0,1,2,3]))
-        print(i, reward)
-        env.show(True)
-        if done:
-            break
+    def __init__(self, env):
+        self.env = env
+        self.actions = [i for i in range(len(env.actions))] 
         
-    plt.show()
+        self.alpha = 0.1 # Learning Rate
+        self.gamma = 0.95 # Discount
+        self.eps0 = 1.0 # Epsilon greedy init
+        self.eps0 = 0.1 # Epsilon greedy init
+        
+        self.batch_size = 32
+        self.replay_memory = deque(maxlen=2000)
+        
+        self.input_size = len(self.env.get_current_state()) 
+    
+    def epsilon_greedy_policy(self, state, epsilon):
+        """
+        Select greedy action from model output based on current state with 
+        probability epsilon. With probability 1 - epsilon select random action.
+        """
+        if np.random.rand() < epsilon:
+            return random.choice(self.actions)
+        else:
+            Q_values = self.model.predict(state[np.newaxis])
+            return np.argmax(Q_values)
+    
+    def play_one_step(self, state, epsilon):
+        """
+        Play one action using the DQN and store S A R S' in replay buffer.
+        """
+        action = self.epsilon_greedy_policy(state, epsilon)
+        
+        next_state, reward, done = self.env.step(action)
+        next_state = next_state.reshape((self.input_size,))
+        self.replay_memory.append((state, action, reward, next_state, done))
+        return next_state, reward, done
+    
+    def sample_experiences(self):
+        """
+        Sample a batch from the replay buffer.
+        """
+        indices = np.random.randint(len(self.replay_memory), 
+                                    size=self.batch_size)
+        batch = [self.replay_memory[index] for index in indices]
+        states, actions, rewards, next_states, dones = [
+                np.array([experience[field_index] for experience in batch])
+                for field_index in range(5)]
+        return states, actions, rewards, next_states, dones
+    
+    def training_step(self):
+        """
+        Train the DQN on a batch from the replay buffer.
+        """
+        # Sample a batch of S A R S' from replay memory
+        experiences = self.sample_experiences()
+        states, actions, rewards, next_states, dones = experiences
+        
+        # Compute target Q values from 'next_states'
+        next_Q_values = self.model.predict(next_states)
+        max_next_Q_values = np.max(next_Q_values, axis=1)
+        target_Q_values = (rewards +
+                       (1 - dones) * self.gamma * max_next_Q_values)
+        target_Q_values = target_Q_values.reshape(-1, 1) # Make column vector
+        
+        # Mask to only consider action taken
+        mask = tf.one_hot(actions, 4) # 4 actions
+        # Compute loss and gradient for predictions on 'states'
+        with tf.GradientTape() as tape:
+            all_Q_values = self.model(states)
+            Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, 
+                                     keepdims=True)
+            loss = tf.reduce_mean(self.loss_fn(target_Q_values, Q_values))
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        # Apply gradients
+        self.optimizer.apply_gradients(zip(grads, 
+                                           self.model.trainable_variables))
+        
+    def build_model(self):
+        """
+        Construct the DQN model.
+        """
+        self.model = keras.Sequential([
+                keras.layers.Dense(64, input_shape=(self.input_size,), 
+                                   activation='relu'),
+                keras.layers.Dense(32, activation='relu'),
+                keras.layers.Dense(4)
+                ])
+
+        self.optimizer = keras.optimizers.Adam(lr=1e-2)
+        self.loss_fn = keras.losses.mean_squared_error
+        
+    def train_nn(self, episodes):
+        """
+        Train the network over a range of episodes.
+        """
+        self.build_model()
+        
+        self.rewards = [] 
+        best_reward = -1000
+        n_rewards = 10
+        reward_list = deque([best_reward for _ in range(n_rewards)], maxlen=n_rewards)
+        for episode in range(episodes):
+            # Get flattened initial state
+            #self.env.init_grid()
+            #obs = self.env.state['state'].copy()
+            obs = self.env.reset()
+            obs = obs.reshape((self.input_size,))
+            
+            episode_reward = 0
+            while True:
+                eps = max(self.eps0 - episode / 500, 0.05) # decay epsilon
+                #eps = self.eps0
+                obs, reward, done = self.play_one_step(obs, eps)
+                episode_reward += reward
+                if done:
+                    break
+                
+            self.rewards.append(episode_reward)
+            reward_list.append(episode_reward)
+            avg_reward = sum(reward_list) / n_rewards
+            if avg_reward > best_reward:
+                best_weights = self.model.get_weights()
+                best_reward = avg_reward
+            
+            print("\rEpisode: {}, Reward: {}, Avg Reward {}, eps: {:.3f}".format(
+                episode, episode_reward, avg_reward, eps), end="")
+            
+            if episode > 50: # Wait for buffer to fill up a bit
+                self.training_step()
+        self.model.set_weights(best_weights)
+            
+    def plot_learning_curve(self):
+        """
+        Plot the rewards per episode collected during training
+        """
+        _, ax = plt.subplots()
+        y = np.array(self.rewards).reshape((-1,1))
+        x = np.arange(1, len(y) + 1).reshape((-1,1))
+        data = np.concatenate((x,y), axis=1)
+        np.savetxt('rewards2.csv', data, delimiter=",")
+        ax.plot(x, y)
+        ax.set_xlabel('episode')
+        ax.set_ylabel('reward per episode')   
+    
+    def play_episode(self):
+        """
+        Play one episode using the DQN and display the grid image at each step.
+        """
+        state = self.env.reset()
+    
+        print("Initial State:")
+        self.env.show()
+        i = 0
+        rewards = 0
+        while True:
+            i += 1
+            #qval = self.model.predict(state.reshape(1,self.input_size))
+            action = self.epsilon_greedy_policy(state, 0.05)
+            #action = (np.argmax(qval)) #take action with highest Q-value
+            print('Move #: %s; Taking action: %s' % (i, action))
+            state, reward, done = self.env.step(action)
+            rewards += reward
+            self.env.show()
+            if done:
+                print("Reward: %s" % (rewards,))
+                break
+            
+
+if __name__ == '__main__':
+    np.random.seed(42)
+    tf.random.set_seed(42)
+    
+    # For timing the script
+    start_time = datetime.now()
+    
+    # Instantiate environment
+    item_env = ItemGatheringGridworld()
+    
+    # Instantiate agent (pass in environment)
+    dqn_ag = DQNAgent(item_env)
+    
+    # Train agent
+    dqn_ag.train_nn(2000)
+    dqn_ag.plot_learning_curve()
+    
+    # Play episode with learned DQN
+    dqn_ag.play_episode()
+
+    run_time = datetime.now() - start_time
+    print(f'Run time: {run_time} s')
     
